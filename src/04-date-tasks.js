c965723be9f2f55cee8440f820fdc16fbb101805
criphood <criphood4life@gmail.com>
2022-11-29 00:08:59 +0300

    feat: add some solution date

--- src/04-date-tasks.js
+++ src/04-date-tasks.js
@@ -19,8 +19,8 @@
  *    'Tue, 26 Jan 2016 13:48:02 GMT' => Date()
  *    'Sun, 17 May 1998 03:00:00 GMT+01' => Date()
  */
-function parseDataFromRfc2822(/* value */) {
-  throw new Error('Not implemented');
+function parseDataFromRfc2822(value) {
+  return new Date(value);
 }
 
 /**
@@ -34,8 +34,8 @@ function parseDataFromRfc2822(/* value */) {
  *    '2016-01-19T16:07:37+00:00'    => Date()
  *    '2016-01-19T08:07:37Z' => Date()
  */
-function parseDataFromIso8601(/* value */) {
-  throw new Error('Not implemented');
+function parseDataFromIso8601(value) {
+  return new Date(value);
 }
 
 
@@ -53,10 +53,10 @@ function parseDataFromIso8601(/* value */) {
  *    Date(2012,1,1)    => true
  *    Date(2015,1,1)    => false
  */
-function isLeapYear(/* date */) {
-  throw new Error('Not implemented');
+function isLeapYear(date) {
+  const fullYear = new Date(date).getFullYear();
+  return new Date(fullYear, 1, 29).getDate() === 29;
 }
-
 
 /**
  * Returns the string representation of the timespan between two dates.
@@ -73,9 +73,22 @@ function isLeapYear(/* date */) {
  *    Date(2000,1,1,10,0,0),  Date(2000,1,1,10,0,0,250)     => "00:00:00.250"
  *    Date(2000,1,1,10,0,0),  Date(2000,1,1,15,20,10,453)   => "05:20:10.453"
  */
-function timeSpanToString(/* startDate, endDate */) {
-  throw new Error('Not implemented');
+function timeSpanToString(startDate, endDate) {
+  const start = new Date(startDate);
+  const end = new Date(endDate);
+  console.log(startDate)
+  console.log(endDate)
+  // const hours = end.getHours() - start.getHours();
+  // const minutes = end.getMinutes() - start.getMinutes();
+  // const seconds = end.getSeconds() - start.getSeconds();
+
+  // const head = [hours, minutes];
+  // const tail = [seconds];
+
+  console.log(start)
 }
+
+console.log(timeSpanToString(Date(2000, 1, 1, 10, 0, 0), Date(2000, 1, 1, 11, 0, 0)))
 
 
 /**
