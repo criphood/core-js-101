19afd92a45524ece66877c69771f820a251e01a6
criphood <criphood4life@gmail.com>
2022-11-29 15:15:32 +0300

    feat: add solution date

--- src/04-date-tasks.js
+++ src/04-date-tasks.js
@@ -73,8 +73,17 @@ function isLeapYear(date) {
  *    Date(2000,1,1,10,0,0),  Date(2000,1,1,10,0,0,250)     => "00:00:00.250"
  *    Date(2000,1,1,10,0,0),  Date(2000,1,1,15,20,10,453)   => "05:20:10.453"
  */
-function timeSpanToString(/* startDate, endDate */) {
-  throw new Error('Not implemented');
+function timeSpanToString(startDate, endDate) {
+  const hours = (endDate.getHours() - startDate.getHours()).toString();
+  const minutes = (endDate.getMinutes() - startDate.getMinutes()).toString();
+  const seconds = (endDate.getSeconds() - startDate.getSeconds()).toString();
+  const milliSeconds = (endDate.getMilliseconds() - startDate.getMilliseconds()).toString().padStart(3, '0');
+  const twoDigit = [hours, minutes, seconds].map((item) => {
+    if (item.length === 2) return item;
+    return item.padStart(2, '0');
+  });
+
+  return `${twoDigit[0]}:${twoDigit[1]}:${twoDigit[2]}.${milliSeconds}`;
 }
 
 /**
@@ -93,10 +102,15 @@ function timeSpanToString(/* startDate, endDate */) {
  *    Date.UTC(2016,3,5,18, 0) => Math.PI
  *    Date.UTC(2016,3,5,21, 0) => Math.PI/2
  */
-function angleBetweenClockHands(/* date */) {
-  throw new Error('Not implemented');
+function angleBetweenClockHands(date) {
+  const hours = date.getHours() - 3 > 12 ? date.getHours() - 3 - 12 : date.getHours() - 3;
+  const minutes = date.getMinutes();
+  const hoursAngleFromZero = 30 * hours + 0.5 * minutes;
+  const minutesAngleFromZero = 6 * minutes;
+  const angle = Math.abs(hoursAngleFromZero - minutesAngleFromZero);
+  if (angle > 180) return (Math.PI * (360 - angle)) / 180;
+  return (Math.PI * angle) / 180;
 }
-
 
 module.exports = {
   parseDataFromRfc2822,
